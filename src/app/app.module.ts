250f064a02fff4de0a33fa9a83f14aa3da1f3fe7
Marchell <selliestrydom@gmail.com>
2022-01-11 15:53:06 +0200

    pwa added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BoardComponent } from './components/board/board.component';
 import { BlockComponent } from './components/block/block.component';
 import { GameComponent } from './components/game/game.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,13 @@ import { MenuComponent } from './components/menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
